7d2002e4f8c88ebf99480dcad53f96977259eb4a
Emanuel Mascarenhas Loiola Sampaio <i1858794@infraero.gov.br>
2025-06-28 21:03:00 -0300

    Inclusão do projeto inicial

---
+++ validacao-novo-cnpj/Program.cs
@@ -0,0 +1,18 @@
+﻿using validacao_novo_cnpj;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string validacao1 = Validacao.IsNovoCnpj("13.638.767/0001-92") ? "válido" : "inválido";
+        string validacao2 = Validacao.IsNovoCnpj("33.EMA.SAM/E007-81") ? "válido" : "inválido";
+        string validacao3 = Validacao.IsNovoCnpj("04.KA2.B01/A001-49") ? "válido" : "inválido";
+
+
+        Console.WriteLine($"O CNPJ 13.638.767/0001-92 é {validacao1}"); //CNPJ válido
+
+        Console.WriteLine($"O CNPJ 33.EMA.SAM/E007-81 é {validacao2}"); //CNPJ válido
+
+        Console.WriteLine($"O CNPJ 04.KA2.B01/A001-49 é {validacao3}"); //CNPJ inválido
+    }
+}
---
+++ validacao-novo-cnpj/Validacao.cs
@@ -0,0 +1,61 @@
+﻿namespace validacao_novo_cnpj
+{
+    /// <summary>
+    /// Classe utilizada para validações
+    /// </summary>
+    public static class Validacao
+    {
+        /// <summary>
+        /// Valida um CNPJ conforme novo formato alfanumérico que entrará em vigor em 2026 pela Receita Federal
+        /// </summary>
+        /// <param name="cnpj"></param>
+        /// <returns></returns>
+        public static bool IsNovoCnpj(string cnpj)
+        {
+            //Deixa somente as posições do CNPJ sem barras, pontos, etc
+            cnpj =  !string.IsNullOrEmpty(cnpj) 
+                    ? cnpj.Trim().ToUpper().Replace(".", "").Replace("-", "").Replace("/", "").Replace("\\", "") 
+                    : "";
+
+            if (cnpj.Length != 14)
+            {
+                return false;
+            }
+
+            //Multiplicadores padrão para os dígitos verificadores
+            int[] multipDV1 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multipDV2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            //Pega as posições que serão utilizadas no cálculo dos dígitos verificadores
+            string calcDV1 = cnpj.Substring(0, 12);
+            string calcDV2 = cnpj.Substring(0, 13);
+
+            int soma = 0;
+            int resto = 0;
+
+            //Calculando o primeiro dígito verificador
+            for (int i = 0; i < multipDV1.Length; i++)
+            {
+                soma += (Convert.ToInt32(calcDV1[i]) - 48) * multipDV1[i];
+            }
+
+            resto = (soma % 11);
+
+            string digito1 = (resto <= 1 ? 0 : 11 - resto).ToString();
+
+            soma = 0;
+
+            //Calculando o segundo dígito verificador
+            for (int i = 0; i < multipDV2.Length; i++)
+            {
+                soma += (Convert.ToInt32(calcDV2[i]) - 48) * multipDV2[i];
+            }
+
+            resto = (soma % 11);
+
+            string digito2 = (resto <= 1 ? 0 : 11 - resto).ToString();
+
+            return cnpj.Equals($"{calcDV1}{digito1}{digito2}");
+        }
+    }
+}
